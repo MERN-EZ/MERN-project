d120308bf382c4613f08aef3e62c21c8cbac66d3
Lenmini-Illangakoon <148671502+Lenmini-Illangakoon@users.noreply.github.com>
2024-07-29 21:58:31 +0530

    wip: Add the Admin main request page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import { UserRoleProvider } from "./context/UserRoleContext";
 import NavBar from "./components/NavBar/NavBar";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
+import  AdminRequestsMain from "./pages/Admin/AdminRequestsMain";
+import StudentRequests from './pages/Admin/StudentRequests'; 
+import CreateAssistant from './pages/Admin/CreateAssistant'; 
+import CreateClassroom from './pages/Admin/CreateClassroom';
 import UserRoleToggle from "./components/UserRoleToggle/UserRoleToggle";
 import "./App.css";
 
@@ -21,6 +25,11 @@ function App() {
               <UserRoleToggle />
               <Routes>
                 <Route path="/" element={<Home />} />
+                {/* Admin Requests Page*/}
+                <Route path="/requests" element={<AdminRequestsMain />} />
+                <Route path="/student-requests" element={<StudentRequests/>} />
+                <Route path="/create-assistant" element={<CreateAssistant/>} />
+                <Route path="/create-classroom" element={<CreateClassroom />} />
               </Routes>
             </Router>
           </UserRoleProvider>
