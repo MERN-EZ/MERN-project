6ecc8fcca9dffd5ec21bc8b27043942a2a981a04
PasandiRanga <140192434+PasandiRanga@users.noreply.github.com>
2024-08-08 21:16:32 +0530

    Revert "Page/registration page"

--- src/components/common/Header/tabs.js
+++ src/components/common/Header/tabs.js
@@ -1,32 +1,25 @@
 //
 const teacher_tabs = [
-  { text: 'Overview', url: '/overview', isActive: true },
-  { text: 'Homework', url: '/homework', isActive: false },
-  { text: 'Resources', url: '/resources', isActive: false },
-  { text: 'Announcements', url: '/announcements', isActive: false },
+  { text: "Overview", url: "/overview", isActive: true },
+  { text: "Homework", url: "/homework", isActive: false },
+  { text: "Resources", url: "/resources", isActive: false },
+  { text: "Announcements", url: "/announcements", isActive: false },
 ];
 const assistant_tabs = [
-  { text: 'Overview', url: '/overview', isActive: true },
-  { text: 'Class Schedules', url: '/schedules', isActive: false },
-  { text: 'Attendance', url: '/attendance', isActive: false },
-  { text: 'Payments', url: '/payments', isActive: false },
+  { text: "Overview", url: "/overview", isActive: true },
+  { text: "Class Schedules", url: "/schedules", isActive: false },
+  { text: "Attendance", url: "/attendance", isActive: false },
+  { text: "Payments", url: "/payments", isActive: false },
 ];
 const admin_tabs = [
-  { text: 'Home', url: '/home', isActive: true },
-  { text: 'Requests', url: '/requests', isActive: false },
-  { text: 'Announcements', url: '/announcements', isActive: false },
-  { text: 'Student Management', url: '/StudentManagement', isActive: false },
+  { text: "Home", url: "/home", isActive: true },
+  { text: "Requests", url: "/requests", isActive: false },
+  { text: "Announcements", url: "/announcements", isActive: false },
+  { text: "Student Management", url: "/StudentManagement", isActive: false },
 ];
-
-const guest_tabs = [
-  { text: 'Register', url: '/register', isActive: true },
-  { text: 'Login', url: '/login', isActive: false },
-];
-
 export {
   // student_tabs,
   teacher_tabs,
   assistant_tabs,
   admin_tabs,
-  guest_tabs,
 };
--- src/routes/GuestRoutes.js
+++ src/routes/GuestRoutes.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import GuestHomePage from '../pages/Guest/Home/Home';
-import RegistrationPage from '../pages/Guest/RegistrationPage/RegistrationPage';
-import LoginPage from '../pages/Guest/Login/LoginPage';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import GuestHomePage from "../pages/Guest/Home/Home";
 
 const GuestRoutes = () => (
   <Routes>
     <Route path="/" element={<GuestHomePage />} />
-    <Route path="/register" element={<RegistrationPage />} />
-    <Route path="/login" element={<LoginPage />} />
     {/* Add more guest-specific routes here */}
   </Routes>
 );
