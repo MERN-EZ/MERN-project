20c7316d5e144c3668c04b0f2bc1b6d8dc1c9599
RavindiFernando <140165006+RavindiFernando@users.noreply.github.com>
2024-08-11 21:49:10 +0530

    style: Registration page, Home page

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import './Button.css';
+import './Button.scss';
 
 const Button = ({ text, variant, onClick }) => {
-    const getButtonClass = () => {
-        switch (variant) {
-            case 'secondary':
-                return 'button-secondary';
-            case 'primary':
-                return 'button-primary';
-            case 'alt':
-                return 'button-alt';
-            default:
-                return 'button-primary';
-        }
-    };
+  const getButtonClass = () => {
+    switch (variant) {
+      case 'secondary':
+        return 'button-secondary';
+      case 'primary':
+        return 'button-primary';
+      case 'alt':
+        return 'button-alt';
+      default:
+        return 'button-primary';
+    }
+  };
 
-    return (
-        <button className={`custom-button ${getButtonClass()}`} onClick={onClick}>
-            {text}
-        </button>
-    );
+  return (
+    <button className={`custom-button ${getButtonClass()}`} onClick={onClick}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
