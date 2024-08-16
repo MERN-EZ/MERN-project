422848d35f5b978f32146aefbec9d25be5c71ea7
Sulakshana Karunarathne <sula2001karunarathne@gmail.com>
2024-08-16 15:36:17 +0530

    wip: minor

--- src/routes/AssistantRoutes.js
+++ src/routes/AssistantRoutes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from '../pages/Assistant/Dashboard/Dashboard';
 import NotFound from '../pages/NotFound/NotFound';
+import Payment from '../pages/Assistant/Payment/Payment';
 
 const AssistantRoutes = () => (
   <Routes>
     <Route path="/" element={<Dashboard />} />
     <Route path="*" element={<NotFound />} />
+    <Route path="/payments" element={<Payment />} />
   </Routes>
 );
 
