f72f82b1c2b7f39dec9e279e0042378164fccba5
Sulakshana Karunarathne <sula2001karunarathne@gmail.com>
2024-08-16 14:55:23 +0530

    fix: errors on assistant

--- src/routes/AssistantRoutes.js
+++ src/routes/AssistantRoutes.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import AssistantHomePage from '../pages/Assistant/Home/Home';
+import Dashboard from '../pages/Assistant/Dashboard/Dashboard';
 import NotFound from '../pages/NotFound/NotFound';
 
 const AssistantRoutes = () => (
   <Routes>
-    <Route path="/" element={<AssistantHomePage />} />
+    <Route path="/" element={<Dashboard />} />
     <Route path="*" element={<NotFound />} />
   </Routes>
 );
