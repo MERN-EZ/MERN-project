1d0ed450e382090e34e8de432d7a8e8407ebce3e
PasandiRanga <140192434+PasandiRanga@users.noreply.github.com>
2024-09-01 22:20:08 +0530

    feat: Add staff login functionality

--- src/routes/GuestRoutes.js
+++ src/routes/GuestRoutes.js
@@ -4,12 +4,14 @@ import GuestHomePage from '../pages/Guest/Home/Home';
 import RegistrationPage from '../pages/Guest/RegistrationPage/RegistrationPage';
 import LoginPage from '../pages/Guest/Login/LoginPage';
 import NotFound from '../pages/NotFound/NotFound';
+import StaffLoginPage from '../pages/Guest/staffLogin/StaffLoginPage.js';
 
 const GuestRoutes = () => (
   <Routes>
     <Route path="/" element={<GuestHomePage />} />
     <Route path="/register" element={<RegistrationPage />} />
     <Route path="/login" element={<LoginPage />} />
+    <Route path="/staffLogin" element={<StaffLoginPage />} />
 
     <Route path="*" element={<NotFound />} />
   </Routes>
