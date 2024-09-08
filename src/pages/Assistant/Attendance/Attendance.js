a27c5d87e48d5093a5bc8a23d6b8798f233f79e4
Savith Panamgama <geethikasavith@gmail.com>
2024-09-08 20:35:16 +0530

    Refactor useAssistantLogin to remove unused variable and update header in axios post request

--- src/pages/Assistant/Attendance/Attendance.js
+++ src/pages/Assistant/Attendance/Attendance.js
@@ -1,25 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import './Attendance.scss';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate from React Router
+import { useNavigate } from 'react-router-dom';
+import { useDB } from '../../../context/DatabaseContext';
 
 const Attendance = () => {
-  // const [searchTerm, setSearchTerm] = useState('');
-  // const [searchYear, setSearchYear] = useState('');
   const [searchId, setSearchId] = useState('');
   const [sendId, setSendId] = useState(null);
   const [attendanceData, setAttendanceData] = useState([]);
+  const { DB } = useDB();
 
-  const navigate = useNavigate(); // Initialize useNavigate
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // Fetch data from the backend API
-    const fetchAttendanceData = async () => {
+    if (!sendId) return;
+    const fetchAttendanceData = async (searchId, DB) => {
       try {
         console.log('searchId ', searchId);
         const response = await axios.get(
-          `http://localhost:5000/assistant/attendance/${searchId}`
+          `http://localhost:5000/assistant/attendance/${searchId}`,
+          { headers: { 'db-name': DB } }
         );
+        console.log('response', response);
+        console.log('response', response.data[0]);
+        setAttendanceData(response.data);
         // setAttendanceData(response.data);
       } catch (error) {
         console.error('Error fetching attendance data:', error);
@@ -28,8 +32,8 @@ const Attendance = () => {
       }
     };
 
-    fetchAttendanceData();
-  }, [sendId]);
+    fetchAttendanceData(searchId, DB);
+  }, [sendId, DB]);
 
   const handleIdChange = (e) => {
     setSearchId(e.target.value);
@@ -69,36 +73,32 @@ const Attendance = () => {
         <table className="attendance-table">
           <thead>
             <tr>
-              <th>Student ID</th>
-              <th>Name</th>
-              <th>Year</th>
+              <th>Date</th>
               <th>Attendance</th>
             </tr>
           </thead>
           <tbody>
-            {attendanceData.map((student) => (
-              <tr key={student.studentId}>
-                <td>{student.studentId}</td>
-                <td>{`${student.firstName} ${student.lastName}`}</td>
-                <td>{student.year}</td>
+            {attendanceData.map((record) => (
+              <tr key={record.studentId}>
+                <td>{record.date}</td>
                 <td>
                   <div className="attendance-options">
                     <button
                       className={`attendance-button present ${
-                        student.attendance ? 'active' : 'fade'
+                        record.attendance ? 'active' : 'fade'
                       }`}
                       onClick={() =>
-                        handleToggleAttendance(student.studentId, true)
+                        handleToggleAttendance(record.studentId, true)
                       }
                     >
                       Present
                     </button>
                     <button
                       className={`attendance-button absent ${
-                        !student.attendance ? 'active' : 'fade'
+                        !record.attendance ? 'active' : 'fade'
                       }`}
                       onClick={() =>
-                        handleToggleAttendance(student.studentId, false)
+                        handleToggleAttendance(record.studentId, false)
                       }
                     >
                       Absent
