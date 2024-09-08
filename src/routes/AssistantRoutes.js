760b2f85bc618c167457b34b3c9338a824665a3c
PasandiRanga <140192434+PasandiRanga@users.noreply.github.com>
2024-09-08 16:29:02 +0530

    refactor: Remove commented code and unused imports in NavBar and StaffReg components

--- src/routes/AssistantRoutes.js
+++ src/routes/AssistantRoutes.js
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import Dashboard from '../pages/Assistant/Dashboard/Dashboard';
 import NotFound from '../components/NotFound/NotFound';
 import Payment from '../pages/Assistant/Payment/Payment';
+import Assistanthome from '../pages/Assistant/Dashboard/Dashboard'
 
 const AssistantRoutes = () => (
   <Routes>
-    <Route path="/" element={<Dashboard />} />
-    <Route path="*" element={<NotFound />} />
     <Route path="/payments" element={<Payment />} />
+    <Route path="/stafflogin" element={<Assistanthome />} />
+    <Route path="*" element={<NotFound />} />
+    
   </Routes>
 );
 
